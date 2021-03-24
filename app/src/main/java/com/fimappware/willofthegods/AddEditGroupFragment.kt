9de5a341aba878f01dbd5e8c84c08a0a81da270a
Mike Collins <fixitman.mike@gmail.com>
2021-03-24 00:12:02 -0400

    removed temporary testing code from add edit group fragment
    Signed-off-by: Mike Collins <fixitman.mike@gmail.com>

--- app/src/main/java/com/fimappware/willofthegods/AddEditGroupFragment.kt
+++ app/src/main/java/com/fimappware/willofthegods/AddEditGroupFragment.kt
@@ -1,17 +1,12 @@
 package com.fimappware.willofthegods
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import com.fimappware.willofthegods.data.AppDb
-import com.fimappware.willofthegods.data.Group
-import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -19,25 +14,13 @@ import kotlinx.coroutines.launch
 
 private const val TAG = "MFCAddEditGroupFragment"
 class AddEditGroupFragment : Fragment() {
-    private var groupId = 0L
-    private lateinit var group : Group
+
     private lateinit var vm : GroupViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let{ args ->
-            groupId = args.getLong(GroupListFragment.ARG_GROUP_ID, 0L)
-        } ?: Log.d(TAG, "OnCreate: no arguments")
-
-
-//        if(groupId < 1L) throw(IllegalArgumentException("No Group Passed"))
-        Log.d(TAG, "onCreate: groupId = $groupId")
-        if(groupId < 1L) groupId = 6L
-
         val db = AppDb.getInstance(requireContext())
         vm = ViewModelProvider(requireActivity(),GroupViewModel.Factory(db))[GroupViewModel::class.java]
-
-
 
     }
 
@@ -51,10 +34,7 @@ class AddEditGroupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        lifecycleScope.launch {
-            group = vm.getGroupById(groupId)
-            view.findViewById<Button>(R.id.button_second).text = group.Name
-        }
+
     }
 
 
