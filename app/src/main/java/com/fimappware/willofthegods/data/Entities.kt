886c7799d9f19f4910eac4c13514d10ed7e806f5
Mike Collins <fixitman.mike@gmail.com>
2021-04-01 00:39:18 -0400

    Added Parcelable implementation to Entities
    Signed-off-by: Mike Collins <fixitman.mike@gmail.com>

--- app/src/main/java/com/fimappware/willofthegods/data/Entities.kt
+++ app/src/main/java/com/fimappware/willofthegods/data/Entities.kt
@@ -1,17 +1,21 @@
 package com.fimappware.willofthegods.data
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "Groups")
 data class Group(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name="_id")
     var id : Long,
     var Name : String,
     var Type : Int = 0
-)
+):Parcelable
 
+@Parcelize
 @Entity(tableName = "GroupItems")
 data class GroupItem(
         @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "_id")
@@ -20,7 +24,7 @@ data class GroupItem(
         var itemText : String?,
         var enabled : Boolean = true,
         var imageURI : String?
-)
+): Parcelable
 
 object Triggers{
     val DELETE_GROUP = """
