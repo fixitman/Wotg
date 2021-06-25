873a411c7b4d579ce7d99a071cc027cb7160fcb8
Mike Collins <fixitman.mike@gmail.com>
2021-06-25 12:00:35 -0400

    Updated navigation Remember last tab visited, exit app on back from any top level fragment
    Signed-off-by: Mike Collins <fixitman.mike@gmail.com>

--- app/src/main/java/com/fimappware/willofthegods/ui/MainActivity.kt
+++ app/src/main/java/com/fimappware/willofthegods/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.fimappware.willofthegods.ui
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -15,42 +17,65 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navListener : NavController.OnDestinationChangedListener
+    private lateinit var prefs : SharedPreferences
+    private val tabs = setOf(R.id.numberFragment,R.id.groupListFragment)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        prefs = getPreferences(Context.MODE_PRIVATE)
+
+
 
         //set up navigation on appBar and bottomNav
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         //appBarConfiguration = AppBarConfiguration(navController.graph)
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.numberFragment,R.id.groupListFragment))
+        appBarConfiguration = AppBarConfiguration(tabs)
             //todo change to set of 3 start fragments corresponding to bottom nav destinations)
+
+        val graph = navController.navInflater.inflate(R.navigation.nav_graph)
+        graph.startDestination = prefs.getInt(PREF_START_TAB,R.id.numberFragment)
+        navController.graph = graph
+
+        navListener = NavController.OnDestinationChangedListener{_, destination, _ ->
+            if(destination.id in tabs){
+                prefs.edit().putInt(PREF_START_TAB,destination.id).apply()
+            }
+        }
+
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         findViewById<BottomNavigationView>(R.id.bottomNavigationView).setupWithNavController(navController)
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        return when (item.itemId) {
-//            R.id.action_settings -> true
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(navListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navController.removeOnDestinationChangedListener(navListener)
+    }
 
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if(tabs.contains(navController.currentDestination?.id)) { //if we're on a top level fragment
+            finishAfterTransition()
+        }else{
+            super.onBackPressed()
+        }
+    }
+
+    companion object{
+        private const val PREF_START_TAB = "StartTab"
     }
 
 }
